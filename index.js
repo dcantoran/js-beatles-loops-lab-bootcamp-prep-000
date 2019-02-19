7c27711b34618cc887034f1bcc6623677f70a782
David Cantoran <davidcantoran36@gmail.com>
2019-02-19 00:42:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+
+function theBeatlesPlay(mus, inst) {
+  var arr = [];
+  for(var i = 0; i < mus.length; i ++) {
+    arr.push(`${mus[i]} plays ${inst[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(arr) {
+  while (arr) {
+    console.log(arr + '!!!');
+  }
+  return arr;
+}
